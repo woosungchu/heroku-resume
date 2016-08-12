7a72c70b4919a6164adfab795e737017fcab4033
woosungchu <woosungchu@gmail.com>
2016-08-13 03:31:57 +0900

    static

--- resume/__init__.py
+++ resume/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
+import os 
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 
 db = MongoEngine(app)
 
-app = Flask(__name__)
+app._static_folder = os.path.abspath("static")
+
+app.debug =True
 
 def register_blueprints(app) :
     from resume.views import index
