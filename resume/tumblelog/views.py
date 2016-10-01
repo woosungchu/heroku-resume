3b1beaba711476e6a4ecee670b8e4c61370cdaef
woosungchu <woosungchu@gmail.com>
2016-10-01 20:35:05 +0900

    pagination

--- resume/tumblelog/views.py
+++ resume/tumblelog/views.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, render_template, request , session, redirect , url_for
+from flask import Blueprint, render_template, request , session, redirect , url_for, make_response
 from flask.views import MethodView
 from tumblelog.models import Post
 from flask_mongoengine.wtf import model_form
 from mongoengine.queryset import Q
-
+import json
 
 ROOT = '/tumblelog/'
 tumblelog = Blueprint('tumblelog',__name__,template_folder='_templates'+ROOT)
+PER_PAGE = 10
 STACKS = ['Flask','MongoDB','MongoEngine','jQuery']
 
 class ListView(MethodView):
@@ -15,7 +16,11 @@ class ListView(MethodView):
     
     def get_context(self):
         user = session.get('SESSION_USER',None)
-        posts = Post.objects.filter( Q(hidden=False) | Q(hidden=True,author=user))
+        posts = Post.objects.filter( 
+                    Q(hidden=None) | 
+                    Q(hidden=False) | 
+                    Q(hidden=True,author=user)
+                ).limit(PER_PAGE)
         form = self.form(request.form)
         
         context = {
@@ -41,6 +46,21 @@ class ListView(MethodView):
             
             return redirect(url_for('tumblelog.list'))
         return render_template(ROOT+'list.html', **context)
+
+class MorePage(MethodView):
+    
+    def get(self,page):
+        skip = int(page) if page != None else 0 
+        user = session.get('SESSION_USER',None)
+        posts = Post.objects.filter( 
+                    Q(hidden=None) | 
+                    Q(hidden=False) | 
+                    Q(hidden=True,author=user)
+                ).skip(skip).limit(PER_PAGE)
+        
+        response = make_response(posts.to_json())
+        response.content_type = 'application/json'
+        return response
         
 class DetailView(MethodView):
     
@@ -59,3 +79,4 @@ class DetailView(MethodView):
 
 tumblelog.add_url_rule(ROOT,view_func= ListView.as_view('list'))
 tumblelog.add_url_rule(ROOT+'<title>/',view_func= DetailView.as_view('detail'))
+tumblelog.add_url_rule(ROOT+'page/<page>/',view_func= MorePage.as_view('page'))
